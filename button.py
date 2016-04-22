8a5da1bbdb712f45787425b52bcdd1ffcc07b632
alextappin <alextappin@gmail.com>
2016-04-21 22:51:22 -0700

    fixed bug with calling on and off

--- button.py
+++ button.py
@@ -1,22 +1,23 @@
 import RPi.GPIO as GPIO
-import subprocess
 import time
-
+import os
+import subprocess
+sensor = 23
 GPIO.setmode(GPIO.BCM)
-
-GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+GPIO.setup(sensor, GPIO.IN, GPIO.PUD_DOWN)
 
 stateOn = False
+
 while True:
     input_state = GPIO.input(23)
     if input_state == False:
         print("GPIO pinssssss pushed")
         if stateOn == False:
             subprocess.call(["python", "on.py"])
-            stateOn == True
+            stateOn = True
             print("GPIO pin on Light")
         else:
             subprocess.call(["python", "off.py"])
+            stateOn = False
             print("GPIO pin turn off light")
         time.sleep(0.1)
-        GPIO.cleanup()
