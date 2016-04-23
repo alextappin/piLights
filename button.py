a90375fc59cc1f3158fe1d4bc5864d469033b363
alextappin <alextappin@gmail.com>
2016-04-22 23:35:03 -0700

    removed print statements in the button.py

--- button.py
+++ button.py
@@ -11,13 +11,10 @@ stateOn = False
 while True:
     input_state = GPIO.input(23)
     if input_state == False:
-        print("GPIO pinssssss pushed")
         if stateOn == False:
             subprocess.call(["python", "on.py"])
             stateOn = True
-            print("GPIO pin on Light")
         else:
             subprocess.call(["python", "off.py"])
             stateOn = False
-            print("GPIO pin turn off light")
         time.sleep(.8)
